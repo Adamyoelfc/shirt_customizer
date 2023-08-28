56b4179f6d9a02b0c5440fd4e8eb349736e58f92
Adam <adam@Adams-Air.lan>
2023-08-27 21:09:41 -0400

    fixing package.json

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -56,7 +56,7 @@ const Customizer = () => {
       // call our backend to generate an ai image
       setGeneratingImg(true);
 
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+      const response = await fetch("https://shirt-customizer-q1ri.onrender.com/api/v1/dalle", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
